ed35ec7010eb0ea16b5f280bf78b079c328fa06f
Reza Asad <rayza.asad@gmail.com>
2018-03-18 16:30:03 -0700

    starting the SMO algorithm

--- svm.py
+++ svm.py
@@ -4,12 +4,19 @@ import numpy
 
 class SVM():
 	# Comments later
-	def __init__(self):
-		pass
+	def __init__(self, reg=0.0, dtype=np.float32):
+		self.dtype = dtype
+		self.reg = reg
+		self.params = {}
 
-	def utility(self, X, y):
-		pass
+		# initialize the parameters of the model
+		self.params['alpha'] = None
+		self.params['bias'] = None
+
+	def maximize_utility(self, X, y):
+		num_train  = len(X)
+		self.params['alpha'] = np.zeros(num_train)
+
 
 	def predict(self, X):
 		pass
-		
