6b29a2766532ff8b56a4427c08dd8753234f5fcc
Reza Asad <rayza.asad@gmail.com>
2018-03-20 20:32:02 -0700

    The naive SMO algorithm for linearly seperable case is implemented. Next plan is to clean up and test the code.

--- svm.py
+++ svm.py
@@ -14,15 +14,15 @@ class SVM():
 		self.params['w'] = None
 		self.params['bias'] = None
 
-	def naive_max_utility(self, X, y):
+	def naive_max_utility(self, X, y, epsilon=1e-3):
 		# The utility function is a polynomial of degree 2 in alpha
 		# a * alpha^2 + b * alpha + c
 		# The solution is -b/(2*a)
 		num_train  = len(X)
-		self.params['alpha'] = np.zeros(num_train)[:,np.newaxis]
+		self.params['alpha'] = np.random.rand(num_train)[:,np.newaxis]
 		idx = range(num_train)
 		i = 0
-		while i < 100:
+		while i < 300:
 			m, n = random.sample(idx, 2)
 			if m > n:
 				m, n = n, m
@@ -30,30 +30,50 @@ class SVM():
 			X_subset = X[idx_subset,:]
 			y_subset = y[idx_subset]
 			alpha_subset = self.params['alpha'][idx_subset]
-			x_m = X[m,:][np.newaxis].T
+			x_m = X[m,:][np.newaxis]
 			y_m = y[m,0]
-			x_n = X[n,:][np.newaxis].T
+			x_n = X[n,:][np.newaxis]
 			y_n = y[n,0]
 
 			# The coefficient of alpha^2
-			a = -0.5 * (np.dot(x_m.T, x_m) + np.dot(x_n.T, x_n))
+			a = -0.5 * (np.dot(x_m, x_m.T) + np.dot(x_n, x_n.T))
 			# the coefficient of alpha
 			b = np.ones(len(idx_subset)) - \
-				np.dot(X_subset, x_n) * y_subset * alpha_subset * y_n - \
-				np.dot(X_subset, x_m) * y_subset * alpha_subset * y_n 
+				np.dot(X_subset, x_n.T) * y_subset * alpha_subset * y_n - \
+				np.dot(X_subset, x_m.T) * y_subset * alpha_subset * y_n 
 			b = np.sum(b)
 			si = -np.sum(alpha_subset * y_subset)
 
 			# Updated alpha_m and alpha_n
-			alpha_n = np.maximum(0, -float(b)/(2*a))
+			argmax = -float(b)/(2*a+epsilon)
+			if argmax >= 0:
+				alpha_n = argmax
+			else:
+				alpha_n = 0
 			alpha_m = np.maximum((si - alpha_n * y_n) * y_m, 0)
 			self.params['alpha'][m] = alpha_m
 			self.params['alpha'][n] = alpha_n
 			i += 1
-		print self.params['alpha']
+		self.params['w'] = np.sum(X * y * self.params['alpha'], axis=0)
+
+		positive_X = X[(y==1).reshape(len(X),),:]
+		negative_X = X[(y==-1).reshape(len(X),),:]
+		self.params['bias'] = -0.5 * (np.max(np.dot(self.params['w'], negative_X.T)) + \
+									  np.min(np.dot(self.params['w'], positive_X.T))) 
+		return self.params['w'], self.params['bias']
 
 	def predict(self, X):
 		y_pred = np.dot(X, self.params['w']) + self.params['bias']
 		y_pred[y_pred >= 0] = 1
 		y_pred[y_pred < 0] = -1
 		return y_pred
+
+
+
+
+
+
+
+
+
+
