3e62c580b09c8ebc4785659df90d3ef1d3d9e066
Reza Asad <rayza.asad@gmail.com>
2018-03-23 13:02:52 -0700

    solved the convergance issue by clipping alpha to stay in feasable range

--- svm.py
+++ svm.py
@@ -4,7 +4,7 @@ import random
 
 class SVM():
 	# Comments later
-	def __init__(self, num_train, num_features, reg=0.0, dtype=np.float32):
+	def __init__(self, num_train, num_features, reg=0.0, dtype=np.float64):
 		self.num_train = num_train
 		self.dtype = dtype
 		self.reg = reg
@@ -15,8 +15,8 @@ class SVM():
 		self.params['w'] = np.ones(num_features)
 		self.params['bias'] = 1
 
-	def naive_max_utility(self, X, y, epsilon=1e-3):
-		def kkt(X, y, tol=1e-1):
+	def naive_max_utility(self, X, y, epsilon=1e-5):
+		def kkt(X, y, tol=1e-2):
 			alpha = self.params['alpha']
 			w = self.params['w']
 			bias = self.params['bias']
@@ -25,15 +25,11 @@ class SVM():
 			constraint_neg_is_valid = np.max(constrain_neg) < tol
 
 			alpha_pos = [i for i in range(len(alpha)) if alpha[i] > 0]
-			constraint_zero = 1 - (np.dot(X[alpha_pos,:], w) + bias) * y[alpha_pos]
-			constraint_zero_is_valid = np.sum(np.abs(constraint_zero - tol) > tol) == 0
+			constraint_zero_is_valid = True
+			if len(alpha_pos) > 0:
+				constraint_zero = 1 - (np.dot(X[alpha_pos,:], w) + bias) * y[alpha_pos]
+				constraint_zero_is_valid = np.sum(np.abs(constraint_zero - tol) > tol) == 0
 
-			# if not constraint_neg_is_valid:
-				# print "negative constraint is not valid"
-				# print np.max(constrain_neg)
-			# if not constraint_zero_is_valid:
-				# print "zero constraint is not valid"
-				# print constraint_zero
 			return constraint_zero_is_valid and constraint_neg_is_valid
 
 
@@ -43,49 +39,54 @@ class SVM():
 		idx = range(self.num_train)
 		j = 0
 		while not kkt(X,y):
-			for i in range(self.num_train-2):
-				idx_subset = idx[:i] + idx[i+2:]
-				X_subset = X[idx_subset,:]
-				y_subset = y[idx_subset]
-				alpha_subset = self.params['alpha'][idx_subset]
-				x_m = X[i,:]
-				y_m = y[i]
-				x_n = X[i+1,:]
-				y_n = y[i+1]
+			# if j == 2500:
+				# break
+			m, n = random.sample(idx, 2)
+			if m > n:
+				m,n = n, m
+			idx_subset = idx[:m] + idx[m+1:n] + idx[n+1:]
+			X_subset = X[idx_subset,:]
+			y_subset = y[idx_subset]
+			alpha_subset = self.params['alpha'][idx_subset]
+			x_m = X[m,:]
+			y_m = y[m]
+			x_n = X[n,:]
+			y_n = y[n]
 
-				si = -np.sum(alpha_subset * y_subset)
-				# The coefficient of alpha^2
-				a = np.dot(x_m, x_n) - 0.5 * (np.dot(x_m, x_m) + np.dot(x_n, x_n))
-				# the coefficient of alpha
-				b = np.dot(X_subset, x_m) * y_subset * alpha_subset * y_n - \
-					np.dot(X_subset, x_n) * y_subset * alpha_subset * y_n
-				b = np.sum(b) + 1 - y_n * y_m  + si * y_n * (np.dot(x_m, x_m) - np.dot(x_m, x_n))
+			si = -np.sum(alpha_subset * y_subset)
+			# The coefficient of alpha^2
+			a = np.dot(x_m, x_n) - 0.5 * (np.dot(x_m, x_m) + np.dot(x_n, x_n))
+			# the coefficient of alpha
+			b = np.dot(X_subset, x_m) * y_subset * alpha_subset * y_n - \
+				np.dot(X_subset, x_n) * y_subset * alpha_subset * y_n
+			b = np.sum(b) + 1 - y_n * y_m  + si * y_n * (np.dot(x_m, x_m) - np.dot(x_m, x_n))
 
-				# Updated alpha_m and alpha_n
-				alpha_n = -float(b)/(2*a+epsilon)
-				alpha_m = (si - alpha_n * y_n) * y_m
-				if alpha_n >= 0 and alpha_m < 0:
-					alpha_m = 0
-					alpha_n = np.maximum(si * y_n, 0)
-				elif alpha_n < 0 and alpha_m >=0:
-					alpha_n = 0
-					alpha_m = np.maximum(si * y_m, 0)
-				elif alpha_n < 0 and alpha_m < 0:
-					alpha_n = 0
-					alpha_m = 0
+			# Updated alpha_m and alpha_n
+			alpha_n = -float(b)/(2*a+epsilon)
+			alpha_m = (si - alpha_n * y_n) * y_m
+			if y_m * y_n == 1:
+				gamma = self.params['alpha'][m] + self.params['alpha'][n]
+				alpha_n = np.maximum(np.minimum(alpha_n, gamma), 0)
+				alpha_m = np.maximum(np.minimum(alpha_m, gamma), 0)
 
-				self.params['alpha'][i] = alpha_m
-				self.params['alpha'][i+1] = alpha_n
-				self.params['w'] = np.sum(X * y[:,np.newaxis] * self.params['alpha'][:,np.newaxis], axis=0)
+			elif y_m * y_n == -1:
+				gamma = self.params['alpha'][m] - self.params['alpha'][n]
+				if gamma > 0:
+					alpha_n = np.maximum(alpha_n, 0)
+					alpha_m = np.maximum(alpha_m, gamma)
+				else:
+					alpha_n = np.maximum(alpha_n, -gamma)
+					alpha_m = np.maximum(alpha_m, 0)
 
-				positive_X = X[(y==1),:]
-				negative_X = X[(y==-1),:]
-				self.params['bias'] = -0.5 * (np.max(np.dot(self.params['w'], negative_X.T)) + \
-											  np.min(np.dot(self.params['w'], positive_X.T)))
-				j += 1 
-				# print np.sum(self.params['alpha'] * y), alpha_m * y_m + alpha_n * y_n, si
-			if j > 1300:
-				break
+			self.params['alpha'][m] = alpha_m
+			self.params['alpha'][n] = alpha_n
+			self.params['w'] = np.sum(X * y[:,np.newaxis] * self.params['alpha'][:,np.newaxis], axis=0)
+
+			positive_X = X[(y==1),:]
+			negative_X = X[(y==-1),:]
+			self.params['bias'] = -0.5 * (np.max(np.dot(self.params['w'], negative_X.T)) + \
+										  np.min(np.dot(self.params['w'], positive_X.T)))
+			j += 1 
 		return self.params['alpha'], self.params['w'], self.params['bias']
 
 	def predict(self, X):
