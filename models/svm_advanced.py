6ee82121b20b7d3b781ffed72bafd0362187ef05
Reza Asad <rayza.asad@gmail.com>
2018-04-02 20:11:19 -0700

    Added a list of methods that are going to be overwritten in the SVMAdvanced

--- models/svm_advanced.py
+++ models/svm_advanced.py
@@ -6,6 +6,14 @@ from svm_basic import *
 
 
 class SVMAdvanced(SVM):
+	def __clip(self, m ,n, alpha_m, alpha_n):
+		pass
+
+	def __solver(self):
+		pass
+
+	def __choose_second_alpha(m, pos_alpha):
+		pass
 
 	def train(self, print_every=500):
 		super(SVMAdvanced, self).train(check_linearity=False, print_every=print_every)
